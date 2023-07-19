103c64e7af243eebd84adab297301a4956146558
lzy-Jerry <763626908@qq.com>
2023-07-19 14:32:52 +0800

    feat: 路由

---
+++ src/components/ClientRender.tsx
@@ -0,0 +1,15 @@
+/*
+ * @Author: lzy-Jerry
+ * @Date: 2023-07-19 13:38:04
+ * @LastEditors: lzy-Jerry
+ * @LastEditTime: 2023-07-19 13:38:52
+ * @Description: 
+ */
+'use client'
+export default function ClientRender() { 
+    return <>
+        <div>
+            I am client render component
+        </div>
+    </>
+}
